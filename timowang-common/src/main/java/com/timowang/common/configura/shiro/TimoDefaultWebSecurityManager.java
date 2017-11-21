cf90613b4d5773606e8f9770f368dc68d013f896
whl <1429038521@qq.com>
2017-11-21 17:55:07 +0800

    Shiro 权限开放

---
+++ timowang-common/src/main/java/com/timowang/common/configura/shiro/TimoDefaultWebSecurityManager.java
@@ -0,0 +1,133 @@
+/**
+ * 广州市两棵树网络科技有限公司版权所有
+ * DT Group Technology & commerce Co., LtdAll rights reserved.
+ * <p>
+ * 广州市两棵树网络科技有限公司，创立于2009年。旗下运营品牌洋葱小姐。
+ * 洋葱小姐（Ms.Onion） 下属三大业务模块 [洋葱海外仓] , [洋葱DSP] , [洋葱海外聚合供应链]
+ * [洋葱海外仓]（DFS）系中国海关批准的跨境电商自营平台(Cross-border ecommerce platform)，
+ * 合法持有海外直邮保税模式的跨境电商营运资格。是渠道拓展，平台营运，渠道营运管理，及客户服务等前端业务模块。
+ * [洋葱DSP]（DSP）系拥有1.3亿消费者大数据分析模型。 是基于客户的消费行为，消费轨迹，及多维度云算法(MDPP)
+ * 沉淀而成的精准消费者模型。洋葱DSP能同时为超过36种各行业店铺 及200万个销售端口
+ * 进行多店铺高精度配货，并能预判消费者购物需求进行精准推送。同时为洋葱供应链提供更前瞻的商品采买需求模型 。
+ * [洋葱海外聚合供应链]（Super Supply Chain）由中国最大的进口贸易集团共同
+ * 合资成立，拥有20余年的海外供应链营运经验。并已入股多家海外贸易企业，与欧美澳等9家顶级全球供应商达成战略合作伙伴关系。
+ * 目前拥有835个国际品牌直接采买权，12万个单品的商品供应库。并已建设6大海外直邮仓库，为国内客户提供海外商品采买集货供应，
+ * 跨境 物流，保税清关三合一的一体化模型。目前是中国唯一多模式聚合的海外商品供应链 。
+ * <p>
+ * 洋葱商城：http://m.msyc.cc/wx/indexView?tmn=1
+ * <p>
+ * 洋桃商城：http://www.yunyangtao.com
+ */
+package com.timowang.common.configura.shiro;
+
+/**
+ * @Title: TimoDefaultWebSecurityManager
+ * @Package: com.timowang.common.configura.shiro
+ * @Description:
+ * @Company: 广州市两棵树网络科技有限公司
+ * @Author: WangHongLin timo-wang@msyc.cc
+ * @Date: 2017/11/21
+ * @Version: V2.0.10
+ * @Modify-by: WangHongLin timo-wang@msyc.cc
+ * @Modify-date: 2017/11/21
+ * @Modify-version: 2.1.5
+ * @Modify-description: 新增：增，删，改，查方法
+ */
+
+import org.apache.shiro.mgt.SubjectDAO;
+import org.apache.shiro.realm.Realm;
+import org.apache.shiro.session.mgt.SessionContext;
+import org.apache.shiro.session.mgt.SessionKey;
+import org.apache.shiro.session.mgt.SessionManager;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.SubjectContext;
+import org.apache.shiro.web.mgt.DefaultWebSecurityManager;
+
+import java.util.Collection;
+
+/**
+ * @ClassName: TimoDefaultWebSecurityManager
+ * @Description:
+ * @Company: 广州市两棵树网络科技有限公司
+ * @Author: WangHonglin timo-wang@msyc.cc
+ * @Date: 2017/11/21
+ */
+public class TimoDefaultWebSecurityManager extends DefaultWebSecurityManager{
+
+    public TimoDefaultWebSecurityManager() {
+        super();
+    }
+
+    public TimoDefaultWebSecurityManager(Realm singleRealm) {
+        super(singleRealm);
+    }
+
+    public TimoDefaultWebSecurityManager(Collection<Realm> realms) {
+        super(realms);
+    }
+
+    @Override
+    protected SubjectContext createSubjectContext() {
+        return super.createSubjectContext();
+    }
+
+    @Override
+    public void setSubjectDAO(SubjectDAO subjectDAO) {
+        super.setSubjectDAO(subjectDAO);
+    }
+
+    @Override
+    protected void afterSessionManagerSet() {
+        super.afterSessionManagerSet();
+    }
+
+    @Override
+    protected SubjectContext copy(SubjectContext subjectContext) {
+        return super.copy(subjectContext);
+    }
+
+    @Override
+    public String getSessionMode() {
+        return super.getSessionMode();
+    }
+
+    @Override
+    public void setSessionMode(String sessionMode) {
+        super.setSessionMode(sessionMode);
+    }
+
+    @Override
+    public void setSessionManager(SessionManager sessionManager) {
+        super.setSessionManager(sessionManager);
+    }
+
+    @Override
+    public boolean isHttpSessionMode() {
+        return super.isHttpSessionMode();
+    }
+
+    @Override
+    protected SessionManager createSessionManager(String sessionMode) {
+        return super.createSessionManager(sessionMode);
+    }
+
+    @Override
+    protected SessionContext createSessionContext(SubjectContext subjectContext) {
+        return super.createSessionContext(subjectContext);
+    }
+
+    @Override
+    protected SessionKey getSessionKey(SubjectContext context) {
+        return super.getSessionKey(context);
+    }
+
+    @Override
+    protected void beforeLogout(Subject subject) {
+        super.beforeLogout(subject);
+    }
+
+    @Override
+    protected void removeRequestIdentity(Subject subject) {
+        super.removeRequestIdentity(subject);
+    }
+}
